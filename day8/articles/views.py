6a1b578e847fd921ecede382b46c101fed71d9ba
dwlim-nota <donguk.lim@nota.ai>
2021-06-02 10:15:51 +0900

    day8

--- day8/articles/views.py
+++ day8/articles/views.py
@@ -16,3 +16,30 @@ def create_proc(request):
     content = request.POST.get("content")
     Article.objects.create(title=title, content=content)
     return redirect("/")
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {}
+    context["article"] = article
+    return render(request, "articles/detail.html", context=context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {}
+    context["article"] = article
+    return render(request, "articles/update.html", context=context)
+
+def update_proc(request, pk):
+    title = request.POST.get("title")
+    content = request.POST.get("content")
+    article = Article.objects.get(pk=pk)
+    article.title = title
+    article.content = content
+    article.save()
+
+    return redirect("/")
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    return redirect("/")
