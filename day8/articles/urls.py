f51c7eaaac61a8139886cb5317ca9597b49f9e8a
dwlim-nota <donguk.lim@nota.ai>
2021-06-02 10:15:51 +0900

    day8

--- day8/articles/urls.py
+++ day8/articles/urls.py
@@ -2,7 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # index
     path('', views.index),
+
+    # create
     path('create/', views.create),
     path('create_proc/', views.create_proc),
+
+    # read
+    path('<int:pk>/', views.detail),
+
+    # update
+    path('<int:pk>/update/', views.update),
+    path('<int:pk>/update_proc/', views.update_proc),
+
+    # delete
+    path('<int:pk>/delete/', views.delete),
 ]
