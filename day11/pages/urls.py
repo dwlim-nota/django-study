1f624a412e4a54ddc84c1afce7da9016cd8d8f60
dwlim-nota <donguk.lim@nota.ai>
2021-06-04 12:24:48 +0900

    add day 11 1:N relation

--- day11/pages/urls.py
+++ day11/pages/urls.py
@@ -5,8 +5,15 @@ app_name = "pages"
 
 urlpatterns = [
     path('', views.index, name="index"),
+
+    # page
     path('create/', views.create, name="create"),
     path('<int:pk>/', views.detail, name="detail"),
     path('<int:pk>/update/', views.update, name="update"),
     path('<int:pk>/delete/', views.delete, name="delete"),
+
+    # comment crud
+    path('<int:page_pk>/comments/create', views.create_comment, name="create"),
+    path('<int:page_pk>/comments/<int:comment_id>/update', views.update, name="update"),
+    path('<int:page_pk>/comments/<int:comment_id>/delete/', views.delete, name="delete"),
 ]
