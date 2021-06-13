152b76ba2c6487860b1601725c6477cd33b7ecfe
noelbird <lduldu00228@gmail.com>
2021-06-13 14:17:15 +0900

    update day9

---
+++ day9/articles/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"id: {self.pk}, title: {self.title}"
